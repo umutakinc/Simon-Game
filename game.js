bd7ea4c5b047df46ac6ff7e656228b57c7fc5231
Umut Akıncı <umut.akinci@medyasoft.com.tr>
2023-11-16 10:47:32 +0300

    nextSequence function add.

---
+++ game.js
@@ -0,0 +1,14 @@
+let buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+
+function nextSequence() {
+    let randomNumber = Math.floor(Math.random() * 4);
+    let randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour)
+
+    $(`#${randomChosenColour}`).fadeOut(100).fadeIn(100);
+    
+    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    audio.play();
+}
+
